986c980e71809f665d70b525fecb6d88ad1a8e6a
pariasdf <maruasdr@gmail.com>
2021-11-18 23:12:00 +0700

    Add project files.

---
+++ JobReport/Program.cs
@@ -0,0 +1,253 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using jobReport.DataServices;
+using Oracle.ManagedDataAccess.Client;
+//using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace JobReport
+{
+    class Program
+    {
+
+        private static string ConnString = "Data Source=10.232.108.221:1521/WSO2PRD;User Id=AM_ANALYTICS_SHARE;Password=WSO2PRD;Min Pool Size=15;Connection Lifetime=180;";
+        private static string QueryString = "SELECT * FROM cfg_ConsumerPackage where packagename='{0}'"; // <----
+
+        static void Main_old(string[] args)
+        {
+            bool isVPN = false;
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+                foreach (NetworkInterface Interface in interfaces)
+                {
+                    if (Interface.OperationalStatus == OperationalStatus.Up && Interface.Description.Contains("Cisco"))
+                    {
+                        isVPN = true;
+                        break;
+                    }
+                }
+            }
+            if (!isVPN) {
+                //doVPN();
+            }
+            Chart Chart1 = new Chart();
+
+            DateTime dt = DateTime.Now;
+            string startDate = new DateTime(dt.Year, dt.Month - 1, 1).ToString("MM/dd/yyyy");
+            string endDate = new DateTime(dt.Year, dt.Month - 1, DateTime.DaysInMonth(dt.Year,dt.Month-1)).ToString("MM/dd/yyyy");
+            var z = new AnalyticsData().GetApplicationUsage(startDate, endDate);
+            //var z = new AnalyticsData().GetApplicationUsage("10/1/2021", "10/31/2021");
+            Chart1.Size = new System.Drawing.Size(800, 600);
+            string[] x = (from p in z
+                          orderby p.HIT ascending
+                          select p.ApplicationName).ToArray();
+
+            //Get the Total of Orders for each City.
+            int[] y = (from p in z
+                       orderby p.HIT ascending
+                       select p.HIT).ToArray();
+            Chart1.Series.Add("Application Usage");
+            Chart1.Series[0].ChartType = SeriesChartType.Pie;
+            Chart1.Series[0].Points.DataBindXY(x, y);
+            Chart1.Series[0].CustomProperties = "PieLabelStyle=Outside";
+            Chart1.ChartAreas.Add("ss");
+            Chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+            Chart1.ChartAreas[0].Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            Chart1.SaveImage(ms, ChartImageFormat.Jpeg);
+            ms.Seek(0, System.IO.SeekOrigin.Begin);
+            iTextSharp.text.Image pic = iTextSharp.text.Image.GetInstance(ms);
+                if (pic.Height > pic.Width)
+                {
+                    //Maximum height is 800 pixels.
+                    float percentage = 0.0f;
+                    percentage = 450 / pic.Height;
+                    pic.ScalePercent(percentage * 100);
+                }
+                else
+                {
+                    //Maximum width is 600 pixels.
+                    float percentage = 0.0f;
+                    percentage = 350 / pic.Width;
+                    pic.ScalePercent(percentage * 100);
+                }
+            Font bold = new Font(Font.FontFamily.COURIER, 9f, Font.NORMAL, new BaseColor(163, 21, 21));
+            PdfPTable PdfTable = new PdfPTable(3);//#1
+            float[] tbwidths = { 50f, 50f, 50f };
+            PdfTable.SetWidths(tbwidths);
+            PdfTable.WidthPercentage = 100;
+            PdfPCell PdfCell = null;
+            PdfCell = new PdfPCell(new Phrase(new Chunk("1", bold)));//#2
+            PdfCell.Border = 0;
+            PdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            PdfTable.AddCell(PdfCell);
+            PdfCell = new PdfPCell(new Phrase(new Chunk("2", bold)));//#3
+            PdfCell.Border = 0;
+            PdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            PdfTable.AddCell(PdfCell);
+            PdfCell = new PdfPCell(new Phrase(new Chunk("3", bold)));//#4
+            PdfCell.Border = 0;
+            PdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            PdfTable.AddCell(PdfCell);
+            Document document = new Document(PageSize.A4);
+            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            PdfWriter.GetInstance(document, fs);
+            document.Open();
+            document.Add(PdfTable);
+            document.Add(pic);
+            document.Close();
+
+
+            //smtp sendmail boripan.c@,tawit.c@,arisa.p@ ,zpanuwat.p@,zphumet.a@,parinthorn.k@
+        }
+
+        private static void TestMod()
+        {
+
+
+            var bold = new Font(Font.FontFamily.COURIER, 9f, Font.NORMAL, new BaseColor(163, 21, 21));
+            var PdfTable = new PdfPTable(3);
+            var tbwidths = new float[] { 50f, 50f, 50f };
+            PdfTable.SetWidths(tbwidths);
+            PdfTable.WidthPercentage = 100;
+            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("1", bold)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_CENTER
+            });
+            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("22", bold)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_CENTER
+            });
+            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("333", bold)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_CENTER
+            });
+
+
+
+
+            // write PDF file
+            var document = new Document(PageSize.A4);
+            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            PdfWriter.GetInstance(document, fs);
+            document.Open();
+            document.Add(PdfTable);
+            document.Close();
+        }
+
+        private static double GetUnix(DateTime date)
+        {
+            return (double)(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds);
+        }
+
+        public static void Main(string[] args)
+        {
+
+            var conn_string_1 = "Data Source=10.232.108.221:1521/WSO2PRD;User Id=AM_ANALYTICS_SHARE;Password=WSO2PRD;Min Pool Size=15;Connection Lifetime=180;"; ;
+            /*
+                        //var connection_string = "Data Source=10.232.108.221,1521; Database=WSO2PRD; User Id=AM_ANALYTICS_SHARE; Password=WSO2PRD;";
+
+                        var conn = new OracleConnection(conn_string_1);
+
+                        conn.Open();
+
+                        conn.Close();*/
+
+
+
+            OracleConnection con = new OracleConnection();
+
+            //using connection string attributes to connect to Oracle Database
+            con.ConnectionString = "Data Source=10.232.108.221:1521/WSO2PRD;User Id=AM_ANALYTICS_SHARE;Password=WSO2PRD;Min Pool Size=15;Connection Lifetime=180;"; ;
+            con.Open();
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+
+
+
+
+
+            double unix_start = GetUnix(DateTime.Now.AddDays(-1));
+
+            double unix_stop = GetUnix(DateTime.Now);
+
+
+            string cmdQuery = "SELECT ApplicationDisplayName as ApplicationName, COUNT(ApplicationDisplayName) as HIT FROM APIRequestLog INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname=apirequestlog.applicationname and apirequestlog.applicationowner = cfg_applicationdisplayname.applicationowner WHERE requesttimestamp between {0:00} and {1:00}  GROUP bY ApplicationDisplayName";
+
+            cmdQuery = string.Format(cmdQuery, unix_start, unix_stop);
+
+
+            // Create the OracleCommand
+            OracleCommand cmd = new OracleCommand(cmdQuery);
+
+            cmd.Connection = con;
+            //cmd.CommandType = CommandType.Text;
+
+            // Execute command, create OracleDataReader object
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                // output Employee Name and Number
+                Console.WriteLine("Employee Name : " + reader.GetString(0) + " , ");
+                  //"Employee Number : " + reader.GetDecimal(1));
+            }
+
+            // Clean up
+            reader.Dispose();
+            cmd.Dispose();
+
+
+
+
+
+            // Close and Dispose OracleConnection object
+            con.Close();
+            con.Dispose();
+            Console.WriteLine("Disconnected");
+
+
+        }
+
+
+
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+
+            return pingable;
+        }
+    }
+}
---
+++ jobReport.DataServices/Class1.cs
@@ -0,0 +1,317 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace jobReport.DataServices
+{
+    public class AnalyticsData
+    {
+        public List<Package> GetPackage(string package)
+        {
+            string query = string.Format("SELECT * FROM cfg_ConsumerPackage where packagename='{0}' ", package);
+            DataTable dt = ExecuteQuery(query);
+            List<Package> appUsage = new List<Package>();
+            appUsage = (from DataRow dr in dt.Rows
+                        select new Package()
+                        {
+                            PackageName = dr["PackageName"].ToString(),
+                            APILIMITSIZE = int.Parse(dr["APIESBLIMIT"].ToString()),
+                            APIPRICE = int.Parse(dr["APIESBPRICE"].ToString()),
+                            OVEREXCEEDPRICE = double.Parse(dr["OVEREXCEEDPRICE"].ToString())
+                        }).ToList();
+            return appUsage;
+        }
+        public List<PriceStruct> GetPriceStructer(string package, string from, string to)
+        {
+            List<PriceStruct> appUsage = new List<PriceStruct>();
+            List<ApplicationUsage> getTotal = GetApplicationUsage(from, to);
+            double x = getTotal.Select(y => y.HIT).Sum();
+            if (!package.Equals("Unlimited"))
+            {
+                string query = string.Format("SELECT SUM(AGG_SUM_SUCCESSCOUNT) as Total,packagename,APIESBLimit,APIESBPRICE,OVEREXCEEDPRICE,CASE WHEN(SUM(AGG_SUM_SUCCESSCOUNT) > APIESBLimit) THEN(SUM(AGG_SUM_SUCCESSCOUNT) - APIESBLimit) ELSE 0 END as OVERLIMIT,CASE WHEN(SUM(AGG_SUM_SUCCESSCOUNT) > APIESBLimit) THEN(SUM(AGG_SUM_SUCCESSCOUNT) - APIESBLimit) * OVEREXCEEDPRICE ELSE 0 END as OVERLIMITPRICE,CASE WHEN(SUM(AGG_SUM_SUCCESSCOUNT) > APIESBLimit) THEN((SUM(AGG_SUM_SUCCESSCOUNT) - APIESBLimit) * OVEREXCEEDPRICE) + APIESBPRICE ELSE APIESBPRICE END as TotalPrice FROM APIM_REQCOUNTAGG_Days INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname = APIM_REQCOUNTAGG_Days.applicationname  LEFT JOIN cfg_consumerpackage ON cfg_consumerpackage.packagename = '{0}' WHERE AGG_TIMESTAMP between {1} and {2} GROUP BY cfg_consumerpackage.packagename, APIESBLimit, OVEREXCEEDPRICE, APIESBPRICE", package, getTimestamp(from), getEndTimestamp(to));
+                DataTable dt = ExecuteQuery(query);
+                if (dt.Rows.Count > 0)
+                    appUsage = (from DataRow dr in dt.Rows
+                                select new PriceStruct()
+                                {
+                                    PackageName = dr["PackageName"].ToString(),
+                                    APIESBLimit = int.Parse(dr["APIESBLIMIT"].ToString()).ToString(),
+                                    OVERLIMIT = int.Parse(dr["OVERLIMIT"].ToString()),
+                                    OVERLIMITPRICE = double.Parse(dr["OVERLIMITPRICE"].ToString()),
+                                    TotalPrice = double.Parse(dr["TotalPrice"].ToString()),
+                                    Total = (int)x,
+                                    PackagePrice = int.Parse(dr["APIESBPRICE"].ToString()),
+                                    OVEREXCEEDPRICE = double.Parse(dr["OVEREXCEEDPRICE"].ToString())
+                                }).ToList();
+                else
+                {
+                    query = string.Format("SELECT 0 as Total,packagename,APIESBLimit,APIESBPRICE,OVEREXCEEDPRICE,0 as OVERLIMIT,0 as OVERLIMITPRICE, APIESBPRICE as TotalPrice FROM  cfg_consumerpackage WHERE packagename = '{0}'", package);
+                    dt = ExecuteQuery(query);
+                    appUsage = (from DataRow dr in dt.Rows
+                                select new PriceStruct()
+                                {
+                                    PackageName = dr["PackageName"].ToString(),
+                                    APIESBLimit = int.Parse(dr["APIESBLIMIT"].ToString()).ToString(),
+                                    OVERLIMIT = int.Parse(dr["OVERLIMIT"].ToString()),
+                                    OVERLIMITPRICE = double.Parse(dr["OVERLIMITPRICE"].ToString()),
+                                    TotalPrice = double.Parse(dr["TotalPrice"].ToString()),
+                                    Total = (int)x,
+                                    PackagePrice = int.Parse(dr["APIESBPRICE"].ToString()),
+                                    OVEREXCEEDPRICE = double.Parse(dr["OVEREXCEEDPRICE"].ToString())
+                                }).ToList();
+                }
+
+            }
+            else
+            {
+                int Month = int.Parse(from.Split('/')[0]);
+                string query = string.Format("SELECT PRICE FROM CFG_UNILIMITEDCHARGE WHERE MONTH = " + Month.ToString());
+                DataTable dt = ExecuteQuery(query);
+                DataRow dr = dt.Rows[0];
+                double TotalPrice = double.Parse(dr["PRICE"].ToString());
+                query = string.Format("SELECT SUM(AGG_SUM_SUCCESSCOUNT) as Total,packagename  FROM APIM_REQCOUNTAGG_Days  LEFT JOIN cfg_consumerpackage ON cfg_consumerpackage.packagename = '{0}' WHERE AGG_TIMESTAMP between {1} and {2} GROUP BY cfg_consumerpackage.packagename, APIESBLimit, OVEREXCEEDPRICE, APIESBPRICE", package, getTimestamp(from), getEndTimestamp(to));
+                dt = ExecuteQuery(query);
+                appUsage = (from DataRow dx in dt.Rows
+                            select new PriceStruct()
+                            {
+                                PackageName = "Unlimited",
+                                Total = (int)x,
+                                APIESBLimit = "Unlimited ",
+                                TotalPrice = TotalPrice,
+                                PackagePrice = TotalPrice
+                            }).ToList();
+            }
+            return appUsage;
+        }
+        public List<ApplicationUsage> GetApplicationUsage(string from, string to)
+        {
+            string query = string.Format("SELECT ApplicationDisplayName as ApplicationName, COUNT(ApplicationDisplayName) as HIT FROM APIRequestLog INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname=apirequestlog.applicationname and apirequestlog.applicationowner = cfg_applicationdisplayname.applicationowner WHERE requesttimestamp between {0} and {1}  GROUP bY ApplicationDisplayName", getTimestamp(from), getEndTimestamp(to));
+            DataTable dt = ExecuteQuery(query);
+            List<ApplicationUsage> appUsage = new List<ApplicationUsage>();
+            appUsage = (from DataRow dr in dt.Rows
+                        select new ApplicationUsage()
+                        {
+                            ApplicationName = dr["ApplicationName"].ToString(),
+                            HIT = int.Parse(dr["HIT"].ToString())
+                        }).ToList();
+            return appUsage;
+        }
+        public List<Error> GetErrorList(string from, string to, int pageSize, int PageNo)
+        {
+            string query = string.Format("SELECT TO_CHAR( FROM_TZ( CAST(DATE '1970-01-01' + (1/24/60/60/1000) *(requesttimestamp) AS TIMESTAMP), SESSIONTIMEZONE), 'MM/DD/YYYY HH24:MI') as requesttime,applicationname,apiname,apiresourcepath,responsecode from apirequestlog where responsecode <>200 and requesttimestamp between {0} and {1} order by requesttimestamp desc", getTimestamp(from), getEndTimestamp(to));
+            DataTable dt = ExecuteQuery(query);
+            List<Error> appUsage = new List<Error>();
+            if (dt.Rows.Count != 0)
+            {
+                appUsage = (from DataRow dr in dt.Rows
+                            select new Error()
+                            {
+                                applicationname = dr["applicationname"].ToString(),
+                                requesttime = dr["requesttime"].ToString(),
+                                responsecode = int.Parse(dr["responsecode"].ToString()),
+                                apiname = dr["apiname"].ToString(),
+                                apiresourcepath = dr["apiresourcepath"].ToString()
+                            }).Skip(pageSize * (PageNo - 1))
+      .Take(pageSize).ToList();
+            }
+            return appUsage;
+        }
+        public List<avgStatistic> GetAverageStatic(string from, string to)
+        {
+            string query = string.Format("SELECT ROUND(AVG(SERVICETIME),2)as avgservice,ROUND(AVG(Backendtime),2) as avgbackend,ROUND(AVG(Responsetime),2)as avgresponse FROM APIRequestLog WHERE requesttimestamp between {0} and {1}", getTimestamp(from), getEndTimestamp(to));
+            DataTable dt = ExecuteQuery(query);
+
+            List<avgStatistic> appUsage = new List<avgStatistic>();
+            if (dt.Rows.Count > 0)
+                appUsage = (from DataRow dr in dt.Rows
+                            select new avgStatistic()
+                            {
+                                avgbackend = (dr["avgbackend"].ToString() == "") ? 0 : double.Parse(dr["avgbackend"].ToString()),
+                                avgresponse = (dr["avgresponse"].ToString() == "") ? 0 : double.Parse(dr["avgresponse"].ToString()),
+                                avgservice = (dr["avgservice"].ToString() == "") ? 0 : double.Parse(dr["avgservice"].ToString())
+                            }).ToList();
+            return appUsage;
+        }
+        public List<avgStatistic> getCPU(string from, string to)
+        {
+            string query = string.Format("SELECT ROUND(AVG(SERVICETIME),2)as avgservice,ROUND(AVG(Backendtime),2) as avgbackend,ROUND(AVG(Responsetime),2)as avgresponse FROM APIRequestLog WHERE requesttimestamp between {0} and {1}", getTimestamp(from), getEndTimestamp(to));
+            DataTable dt = ExecuteQuery(query);
+
+            List<avgStatistic> appUsage = new List<avgStatistic>();
+            if (dt.Rows.Count > 0)
+                appUsage = (from DataRow dr in dt.Rows
+                            select new avgStatistic()
+                            {
+                                avgbackend = double.Parse(dr["avgbackend"].ToString()),
+                                avgresponse = double.Parse(dr["avgresponse"].ToString()),
+                                avgservice = double.Parse(dr["avgservice"].ToString())
+                            }).ToList();
+            return appUsage;
+        }
+        public double getTimestamp(string dateTime)
+        {
+            DateTime oDate = Convert.ToDateTime(dateTime);
+            return (oDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds * 1000;
+        }
+        public double getEndTimestamp(string dateTime)
+        {
+            DateTime oDate = Convert.ToDateTime(dateTime);
+            oDate = oDate.AddDays(1);
+            return ((oDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds * 1000) - 1;
+        }
+
+        public List<ResponseHit> getResponseCode(string from, string to)
+        {
+            string query = string.Format("SELECT requesttimestamp,responsecode,servicetime FROM apirequestlog WHERE requesttimestamp between {0} and {1} ", getTimestamp(from), getEndTimestamp(to));
+            DataTable dt = ExecuteQuery(query);
+            List<Response> appUsage = new List<Response>();
+            appUsage = (from DataRow dr in dt.Rows
+                        select new Response()
+                        {
+                            responseCode = int.Parse(dr["responsecode"].ToString()),
+                            requestTimestamp = double.Parse(dr["requesttimestamp"].ToString()),
+                            AvgServiceTime = int.Parse(dr["servicetime"].ToString())
+                        }).ToList();
+            var groups = appUsage.GroupBy(x =>
+            {
+                var stamp = x.requestTimestamp;
+                stamp = stamp - (stamp % (5000 * 60));
+                return stamp;
+            })
+.Select(g => new ResponseHit { requestTimestamp = g.Key, HIT = g.Count(s => s.responseCode == 200), AvgServiceTime = string.Format("{0:n}", g.Average(x => x.AvgServiceTime)) })
+.OrderBy(x => x.requestTimestamp)
+.ToList();
+            return groups;
+        }
+        private DateTime UnixToDateTime(double unixTimeStamp)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp / 1000).ToLocalTime();
+            return dtDateTime;
+        }
+        private DataTable ExecuteQuery(string query)
+        {
+            string strConnection = ConfigurationManager.ConnectionStrings["oracleserver"].ToString();
+            using (OracleConnection conn = new OracleConnection(strConnection))
+            {
+                using (OracleCommand command = new OracleCommand(query, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        OracleDataAdapter da = new OracleDataAdapter(query, conn);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "Table1");
+                        return ds.Tables[0];
+                    }
+                    catch (OracleException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return null;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+        private DataTable ExecuteQueryMetric(string query)
+        {
+            string strConnection = ConfigurationManager.ConnectionStrings["ORMetric"].ToString();
+            using (OracleConnection conn = new OracleConnection(strConnection))
+            {
+                using (OracleCommand command = new OracleCommand(query, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        OracleDataAdapter da = new OracleDataAdapter(query, conn);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "Table1");
+                        return ds.Tables[0];
+                    }
+                    catch (OracleException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return null;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+    }
+    public class ApplicationUsage
+    {
+        public string ApplicationName { get; set; }
+        public int HIT { get; set; }
+    }
+
+    public class Timeline
+    {
+        public int StatusCode { get; set; }
+        public int HIT { get; set; }
+        public string Time { get; set; }
+    }
+
+    public class Response
+    {
+        public double requestTimestamp { get; set; }
+        public int responseCode { get; set; }
+        public int AvgServiceTime { get; set; }
+    }
+    public class ResponseHit
+    {
+        public double requestTimestamp { get; set; }
+        public int HIT { get; set; }
+
+        public string AvgServiceTime { get; set; }
+    }
+    public class avgStatistic
+    {
+        public double avgservice { get; set; }
+        public double avgbackend { get; set; }
+        public double avgresponse { get; set; }
+    }
+    public class Error
+    {
+        public string requesttime { get; set; }
+        public string applicationname { get; set; }
+        public string apiname { get; set; }
+        public string apiresourcepath { get; set; }
+        public int responsecode { get; set; }
+        public double requesttimestamp { get; set; }
+    }
+    public class Package
+    {
+        public string PackageName { get; set; }
+        public int APILIMITSIZE { get; set; }
+        public int APIPRICE { get; set; }
+        public double OVEREXCEEDPRICE { get; set; }
+    }
+
+    public class PriceStruct
+    {
+        public string PackageName { get; set; }
+        public string APIESBLimit { get; set; }
+        public int OVERLIMIT { get; set; }
+        public double OVERLIMITPRICE { get; set; }
+        public double TotalPrice { get; set; }
+        public int Total { get; set; }
+        public double PackagePrice { get; set; }
+        public double OVEREXCEEDPRICE { get; set; }
+    }
+
+    public class MetricValues
+    {
+        public double TIMESTAMP { get; set; }
+        public double VALUE { get; set; }
+    }
+}
