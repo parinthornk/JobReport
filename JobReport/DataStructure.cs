2b668db92c654fb8d8f6fb62e2d4a5f2e97b8093
pariasdf <maruasdr@gmail.com>
2021-11-23 23:23:46 +0700

    Added Summary Table

---
+++ JobReport/DataStructure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobReport
+{
+    public class AppCountInfo
+    {
+        private class Sort : IComparer<AppCountInfo>
+        {
+            public int Compare(AppCountInfo x, AppCountInfo y)
+            {
+                if (x.Count > y.Count)
+                {
+                    return -1;
+                }
+                if (x.Count < y.Count)
+                {
+                    return +1;
+                }
+                return 0;
+            }
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public int Count { get; set; }
+
+        public static AppCountInfo[] GetData()
+        {
+            return new AppCountInfo[]
+            {
+                new AppCountInfo() { Name = "PTT Smart Procurement", Count = 1776335, },
+                new AppCountInfo() { Name = "PTT Mercury", Count = 26, },
+                new AppCountInfo() { Name = "PTT Vendor Management", Count = 4743, },
+                new AppCountInfo() { Name = "PTT SDS", Count = 4, },
+                new AppCountInfo() { Name = "PTT ICTCM", Count = 2524, },
+                new AppCountInfo() { Name = "PTT NGV Barcode", Count = 649, },
+                new AppCountInfo() { Name = "PTT GSM", Count = 36, },
+                new AppCountInfo() { Name = "PTT Receipt Recording System", Count = 24484, },
+                new AppCountInfo() { Name = "PTT Credit Bureau Database", Count = 89199, },
+                new AppCountInfo() { Name = "PTT Electronic Bank Guarantee", Count = 316769, },
+                new AppCountInfo() { Name = "PTT Standard Price", Count = 16, },
+            };
+        }
+
+        public static void SortArray(AppCountInfo[] app_count_data)
+        {
+            Array.Sort(app_count_data, new Sort());
+        }
+    }
+}
--- JobReport/Program.cs
+++ JobReport/Program.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace JobReport
@@ -112,102 +113,10 @@ namespace JobReport
             //smtp sendmail boripan.c@,tawit.c@,arisa.p@ ,zpanuwat.p@,zphumet.a@,parinthorn.k@
         }
 
-        private static void TestMod()
-        {
-
-
-            var bold = new Font(Font.FontFamily.COURIER, 9f, Font.NORMAL, new BaseColor(163, 21, 21));
-            var PdfTable = new PdfPTable(3);
-            var tbwidths = new float[] { 50f, 50f, 50f };
-            PdfTable.SetWidths(tbwidths);
-            PdfTable.WidthPercentage = 100;
-            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("1", bold)))
-            {
-                Border = 0,
-                HorizontalAlignment = Element.ALIGN_CENTER
-            });
-            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("22", bold)))
-            {
-                Border = 0,
-                HorizontalAlignment = Element.ALIGN_CENTER
-            });
-            PdfTable.AddCell(new PdfPCell(new Phrase(new Chunk("333", bold)))
-            {
-                Border = 0,
-                HorizontalAlignment = Element.ALIGN_CENTER
-            });
-
-
-
-
-            // write PDF file
-            var document = new Document(PageSize.A4);
-            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
-            PdfWriter.GetInstance(document, fs);
-            document.Open();
-            document.Add(PdfTable);
-            document.Close();
-        }
-
         private static double GetUnix(DateTime date)
         {
             return (double)(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds);
         }
-
-        public static void Main(string[] args)
-        {
-
-
-            OracleConnection con = new OracleConnection();
-
-            //using connection string attributes to connect to Oracle Database
-            con.ConnectionString = "Data Source=10.232.108.221:1521/WSO2PRD;User Id=AM_ANALYTICS_SHARE;Password=WSO2PRD;Min Pool Size=15;Connection Lifetime=180;"; ;
-            con.Open();
-            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
-
-            double unix_start = GetUnix(DateTime.Now.AddDays(-1));
-
-            double unix_stop = GetUnix(DateTime.Now);
-
-
-            string cmdQuery = "SELECT ApplicationDisplayName as ApplicationName, COUNT(ApplicationDisplayName) as HIT FROM APIRequestLog INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname=apirequestlog.applicationname and apirequestlog.applicationowner = cfg_applicationdisplayname.applicationowner WHERE requesttimestamp between {0:00} and {1:00}  GROUP bY ApplicationDisplayName";
-
-            cmdQuery = string.Format(cmdQuery, unix_start, unix_stop);
-
-
-            // Create the OracleCommand
-            OracleCommand cmd = new OracleCommand(cmdQuery);
-
-            cmd.Connection = con;
-            //cmd.CommandType = CommandType.Text;
-
-            // Execute command, create OracleDataReader object
-            OracleDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-                // output Employee Name and Number
-                Console.WriteLine("Employee Name : " + reader.GetString(0) + " , ");
-                  //"Employee Number : " + reader.GetDecimal(1));
-            }
-
-            // Clean up
-            reader.Dispose();
-            cmd.Dispose();
-
-
-
-
-
-            // Close and Dispose OracleConnection object
-            con.Close();
-            con.Dispose();
-            Console.WriteLine("Disconnected");
-
-
-        }
-
-
 
         public static bool PingHost(string nameOrAddress)
         {
@@ -233,6 +142,294 @@ namespace JobReport
             }
 
             return pingable;
+        }
+
+        private static void TestOracle()
+        {
+
+            //using connection string attributes to connect to Oracle Database
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = "Data Source=10.232.108.221:1521/WSO2PRD;User Id=AM_ANALYTICS_SHARE;Password=WSO2PRD;Min Pool Size=15;Connection Lifetime=180;"; ;
+            con.Open();
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+
+            var query_1 = "SELECT ApplicationDisplayName as ApplicationName, COUNT(ApplicationDisplayName) as HIT FROM APIRequestLog INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname=apirequestlog.applicationname and apirequestlog.applicationowner = cfg_applicationdisplayname.applicationowner WHERE requesttimestamp between {0:00} and {1:00}  GROUP bY ApplicationDisplayName";
+            var query_2 = "SELECT ROUND(AVG(SERVICETIME),2)as avgservice,ROUND(AVG(Backendtime),2) as avgbackend,ROUND(AVG(Responsetime),2)as avgresponse FROM APIRequestLog WHERE requesttimestamp between {0:00} and {1:00}";
+            var query_3 = "SELECT requesttimestamp,responsecode,servicetime FROM apirequestlog WHERE requesttimestamp between {0:00} and {1:00}";
+
+            string cmdQuery = query_1;
+
+            var date = DateTime.Now;
+            date = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+            date_end = date.AddDays(-date.Day);
+            date_begin = date_end.AddDays(-date_end.Day + 1);
+
+            double unix_start = GetUnix(date_begin);
+            double unix_stop = GetUnix(date_end);
+            cmdQuery = string.Format(cmdQuery, unix_start, unix_stop);
+            Console.WriteLine(cmdQuery);
+
+            // Create the OracleCommand
+            OracleCommand cmd = new OracleCommand(cmdQuery);
+
+            cmd.Connection = con;
+            //cmd.CommandType = CommandType.Text;
+
+            // Execute command, create OracleDataReader object
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            var filed_count = reader.FieldCount;
+
+            var list = new List<string>();
+
+            while (reader.Read())
+            {
+                var text = string.Empty;
+                for (int i = 0; i < filed_count; i++)
+                {
+                    text = text + reader.GetString(i) + ", ";
+                }
+                Console.WriteLine(text);
+
+                list.Add(text);
+            }
+
+            File.WriteAllLines("list.txt", list);
+
+            // Clean up
+            reader.Dispose();
+            cmd.Dispose();
+
+            // Close and Dispose OracleConnection object
+            con.Close();
+            con.Dispose();
+            Console.WriteLine("Disconnected");
+        }
+
+        public static void Main(string[] args)
+        {
+            /*var date_ref = new DateTime(1970, 1, 1);
+            for (; ; )
+            {
+                Console.WriteLine(DateTime.Now.Millisecond);
+                Thread.Sleep(1000);
+            }*/
+
+            //TestOracle();
+            //GetDateBeginAndEnd();
+
+            ReportPDF();
+        }
+
+        private static DateTime date_begin, date_end;
+        private static void GetDateBeginAndEnd()
+        {
+            var date = DateTime.Now;
+            date = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+            date_end = date.AddDays(-date.Day);
+            date_begin = date_end.AddDays(-date_end.Day + 1);
+
+            double unix_start = GetUnix(date_begin);
+            double unix_stop = GetUnix(date_end);
+            string cmdQuery = "SELECT ApplicationDisplayName as ApplicationName, COUNT(ApplicationDisplayName) as HIT FROM APIRequestLog INNER JOIN cfg_applicationdisplayname on cfg_applicationdisplayname.applicationname=apirequestlog.applicationname and apirequestlog.applicationowner = cfg_applicationdisplayname.applicationowner WHERE requesttimestamp between {0:00} and {1:00}  GROUP bY ApplicationDisplayName";
+            cmdQuery = string.Format(cmdQuery, unix_start, unix_stop);
+            Console.WriteLine(cmdQuery);
+        }
+
+        private static void ReportPDF()
+        {
+            // create head table
+            var head_font_style = new Font(Font.FontFamily.HELVETICA, 11f, Font.BOLD, new BaseColor(63, 63, 63));
+            var head_table = new PdfPTable(1);
+            var head_width = new float[] { 100f };
+            head_table.SetWidths(head_width);
+            head_table.WidthPercentage = 40;
+            head_table.HorizontalAlignment = 2;
+            head_table.AddCell(new PdfPCell(new Phrase(new Chunk("Transaction summary by Application", head_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+
+            // create a table
+            var font_style = new Font(Font.FontFamily.HELVETICA, 8f, Font.NORMAL, new BaseColor(63, 63, 63));
+            var table_app_count = new PdfPTable(2);
+            var tbwidths = new float[] { 80f, 40f };
+            table_app_count.SetWidths(tbwidths);
+            table_app_count.WidthPercentage = 40;
+
+            table_app_count.HorizontalAlignment = 2;
+
+            var app_count_data = AppCountInfo.GetData();
+
+            // sort?
+            AppCountInfo.SortArray(app_count_data);
+
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+
+            var count_total = 0;
+
+            foreach (var x in app_count_data)
+            {
+                count_total += x.Count;
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(x.Name, font_style)))
+                {
+                    Border = 0,
+                    HorizontalAlignment = Element.ALIGN_LEFT,
+                });
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Format("{0:n0}", x.Count), font_style)))
+                {
+                    Border = 0,
+                    HorizontalAlignment = Element.ALIGN_RIGHT,
+                });
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+                {
+                    Border = 0,
+                    HorizontalAlignment = Element.ALIGN_RIGHT,
+                });
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+                {
+                    Border = 0,
+                    HorizontalAlignment = Element.ALIGN_RIGHT,
+                });
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, font_style)))
+                {
+                    HorizontalAlignment = Element.ALIGN_RIGHT,
+                    BorderColor = new BaseColor(215, 215, 215),
+                });
+                table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, font_style)))
+                {
+                    HorizontalAlignment = Element.ALIGN_RIGHT,
+                    BorderColor = new BaseColor(215, 215, 215),
+                });
+            }
+
+
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk("Total", font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Format("{0:n0}", count_total), font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, font_style)))
+            {
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+                BorderColor = new BaseColor(215, 215, 215),
+            });
+            table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, font_style)))
+            {
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+                BorderColor = new BaseColor(215, 215, 215),
+            });
+
+            // create foot table
+            var foot_font_style = new Font(Font.FontFamily.HELVETICA, 11f, Font.BOLD, new BaseColor(63, 63, 63));
+            var foot_table = new PdfPTable(1);
+            var foot_width = new float[] { 100f };
+            foot_table.SetWidths(foot_width);
+            foot_table.WidthPercentage = 40;
+            foot_table.HorizontalAlignment = 2;
+            foot_table.AddCell(new PdfPCell(new Phrase(new Chunk(" ", foot_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+            foot_table.AddCell(new PdfPCell(new Phrase(new Chunk(" ", foot_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+            foot_table.AddCell(new PdfPCell(new Phrase(new Chunk("Charging Summary", foot_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+            foot_table.AddCell(new PdfPCell(new Phrase(new Chunk(" ", foot_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+
+            var foot_table_font_style = new Font(Font.FontFamily.HELVETICA, 8f, Font.NORMAL, new BaseColor(63, 63, 63));
+            var foot_table_app_count = new PdfPTable(2);
+            var foot_tbwidths = new float[] { 80f, 40f };
+            foot_table_app_count.SetWidths(foot_tbwidths);
+            foot_table_app_count.WidthPercentage = 40;
+            foot_table_app_count.HorizontalAlignment = 2;
+
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk("Total Usage", foot_table_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+            });
+
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Format("{0:n0}", count_total), foot_table_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", foot_table_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(" ", foot_table_font_style)))
+            {
+                Border = 0,
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+            });
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, foot_table_font_style)))
+            {
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+                BorderColor = new BaseColor(215, 215, 215),
+            });
+            foot_table_app_count.AddCell(new PdfPCell(new Phrase(new Chunk(string.Empty, foot_table_font_style)))
+            {
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+                BorderColor = new BaseColor(215, 215, 215),
+            });
+
+
+
+            // write PDF file
+            var document = new Document(PageSize.A4);
+            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            PdfWriter.GetInstance(document, fs);
+            document.Open();
+            document.Add(new Paragraph("Hello World! 1"));
+            document.Add(new Paragraph("Hello World! 2"));
+            document.Add(new Paragraph("Hello World! 3"));
+            document.Add(head_table);
+            document.Add(table_app_count);
+            document.Add(foot_table);
+            document.Add(foot_table_app_count);
+            document.Add(new Paragraph("Hello World! 4"));
+            document.Add(new Paragraph("Hello World! 5"));
+            document.Add(new Paragraph("Hello World! 6"));
+            document.Close();
         }
     }
 }
